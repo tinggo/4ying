17be3570db1e988477e98bdec01c4981d6a8bd29
yxiao <yuting.xiao@activision.com>
2019-10-18 22:04:02 +0800

    first submit from YXIAO-PC.

--- source/vector.cpp
+++ source/vector.cpp
@@ -1,1 +1,37 @@
+#include "mathutil.h"
+#include "math.h"
 #include "vector.h"
+
+
+float vector3::magnitude()
+{
+	return sqrt( pow( x, 2 ) + pow( y, 2) + pow( z, 2 ) );
+}
+
+
+void vector3::normalize()
+{
+	float length = magnitude();
+	if ( floatEqual( length, 1.0f ) )
+	{
+		return;
+	}
+
+	x = x / length;
+	y = y / length;
+	z = z / length;
+}
+
+
+void vector3::scale( float s )
+{
+	x *= s;
+	y *= s;
+	z *= s;
+}
+
+
+vector3 vector3::operator+ ( const vector3 &rhv )
+{
+
+}
